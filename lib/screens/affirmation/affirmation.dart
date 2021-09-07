d949db8cb56cbf9f25acd639592db350ee57c2f7
Matilda <afemalecoder@gmail.com>
2021-09-07 16:20:51 +0100

    fix #2, refactor app bar

--- lib/screens/affirmation/affirmation.dart
+++ lib/screens/affirmation/affirmation.dart
@@ -3,6 +3,7 @@ library affirmation;
 import 'package:affirmation_gratitude/screens/add_affirmation/add_affirmation.dart';
 import 'package:affirmation_gratitude/screens/quoteScreen/quote.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/diagnostics.dart';
 import 'package:provider/provider.dart';
 
 import 'package:affirmation_gratitude/model/note_operation.dart';
--- lib/screens/affirmation/affirmation_screen.dart
+++ lib/screens/affirmation/affirmation_screen.dart
@@ -9,27 +9,7 @@ class AffirmationScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xff464bbd),
       floatingActionButton: const FloatingButton(),
-      appBar: AppBar(
-        title: const Text('Affirmation'),
-        centerTitle: true,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: IconButton(
-              onPressed: () =>
-                  Navigator.pushNamed(context, QuoteData.routeName),
-              icon: const Icon(Icons.home),
-            ),
-          ),
-        ],
-      ),
+      appBar: const AffirmationAppBar(),
       body: Consumer<NotesOperation>(
         builder: (context, NotesOperation data, child) {
           return ListView.builder(
@@ -43,6 +23,37 @@ class AffirmationScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class AffirmationAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const AffirmationAppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(57);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Affirmation'),
+      centerTitle: true,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: IconButton(
+            onPressed: () => Navigator.pushNamed(context, QuoteData.routeName),
+            icon: const Icon(Icons.home),
+          ),
+        ),
+      ],
     );
   }
 }
