4262f3a0412e716aff9a993baf74dac0aec43e91
Matilda <afemalecoder@gmail.com>
2021-09-28 14:44:08 +0100

    Sort imports

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:affirmation_gratitude/services/quotes_network.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
