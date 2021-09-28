5f9c53443999cfadc71ffce41cba7baf545ac9a6
Matilda <afemalecoder@gmail.com>
2021-09-28 17:40:57 +0100

    Affirmation Screen Layout

--- lib/screens/affirmation/affirmation_screen.dart
+++ lib/screens/affirmation/affirmation_screen.dart
@@ -11,44 +11,89 @@ class AffirmationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: const Padding(
-          padding: EdgeInsets.all(12),
-          child: Text(
-            'Affirmation',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+      body: Column(
+        children: [
+          AffirmationAppBar(
+            showBackButton: showBackButton,
           ),
-        ),
-        leading: showBackButton
-            ? Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.arrow_back_ios_new,
-                    size: 25,
-                  ),
+          Consumer<AffirmationOperation>(
+            builder: (context, AffirmationOperation data, child) {
+              return Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: data.notes.length,
+                  itemBuilder: (context, index) {
+                    return AffirmationCard(
+                      data.notes[index],
+                      key: Key('$index'),
+                    );
+                  },
                 ),
-              )
-            : null,
-        backgroundColor: const Color(0xff8A96F6),
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(40))),
-      ),
-      body: Consumer<AffirmationOperation>(
-        builder: (context, AffirmationOperation data, child) {
-          return ListView.builder(
-            itemCount: data.notes.length,
-            itemBuilder: (context, index) {
-              return AffirmationCard(
-                data.notes[index],
-                key: Key('$index'),
               );
             },
-          );
-        },
+          ),
+        ],
+      ),
+    );
+  }
+}
+// from => home
+// inter => passing by
+// to => arrive to the app bar
+
+class AffirmationAppBar extends StatelessWidget {
+  const AffirmationAppBar({Key? key, this.showBackButton = false})
+      : super(key: key);
+  final bool showBackButton;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 110,
+      width: double.infinity,
+      child: Card(
+        color: const Color(0xff8A96F6),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40),
+            bottomRight: Radius.circular(40),
+          ),
+        ),
+        margin: EdgeInsets.zero,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                if (showBackButton)
+                  InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 30,
+                        color: Color(0xFFFFFFFF),
+                      ),
+                    ),
+                  ),
+                const Text(
+                  'Affirmation',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Color(0xFFFFFFFF),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
