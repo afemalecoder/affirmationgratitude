62cec5decca058fc811b7024be94c64cccedec3e
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-10-06 15:26:11 +0100

    Update navigator_bar.dart

--- lib/core/navigation/navigator_bar.dart
+++ lib/core/navigation/navigator_bar.dart
@@ -41,17 +41,15 @@ class _AffirmationNavigationState extends State<AffirmationNavigation> {
           child: screens[currentIndex],
         ),
         floatingActionButton: FloatingActionButton(
-            backgroundColor:
-                Theme.of(context).floatingActionButtonTheme.backgroundColor,
-            child: Icon(
-              Icons.add,
-              size: 45,
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
-            onPressed: () => context.read<ThemeProvider>().toggleAppTheme()
-            // Navigator.of(context).pushNamed(AddNoteScreen.routeName),
-
-            ),
+          backgroundColor:
+              Theme.of(context).floatingActionButtonTheme.backgroundColor,
+          child: Icon(
+            Icons.add,
+            size: 45,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+          onPressed: () => context.read<ThemeProvider>().toggleAppTheme(),
+        ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         bottomNavigationBar: AffirNavigationBar(
           currentIndex: currentIndex,
