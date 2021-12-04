b0d12f4df1cd8a93cc111883f80b73a059799b23
Matilda <afemalecoder@gmail.com>
2021-12-04 18:49:47 +0000

    chore: Removed dead imports

--- lib/screens/quote/quote.dart
+++ lib/screens/quote/quote.dart
@@ -1,9 +1,9 @@
 library quote;
 
+
 import 'package:affirmation_gratitude/core/widgets/affirmation_app_bar.dart';
 import 'package:affirmation_gratitude/model/quotes.dart';
-
 import 'package:affirmation_gratitude/services/quotes_network.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 part 'quote_screen.dart';
--- lib/services/affirmation_auth.dart
+++ lib/services/affirmation_auth.dart
@@ -1,4 +1,3 @@
-import 'package:affirmation_gratitude/screens/authentication/authentication.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
