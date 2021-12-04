3656084e448df5989b494de97e2e433af6daed3b
Matilda <afemalecoder@gmail.com>
2021-12-04 18:56:26 +0000

    feature[Profile]: Implemented log out and switch
    - Toggle dark mode.
    - fixed imports

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,7 +1,15 @@
 library profile;
 
+import 'package:affirmation_gratitude/core/theme/theme.dart';
+import 'package:affirmation_gratitude/core/widgets/affirmation_app_bar.dart';
 import 'package:affirmation_gratitude/screens/authentication/authentication.dart';
 import 'package:affirmation_gratitude/services/affirmation_auth.dart';
+import 'package:affirmation_gratitude/services/affirmation_network.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 part 'user_profile.dart';
