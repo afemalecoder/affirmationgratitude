dd147643fe9f94b2948f66599be1cef52747621a
Matilda <afemalecoder@gmail.com>
2021-12-16 19:25:43 +0000

    Ui update

--- lib/screens/authentication/app_logo.dart
+++ lib/screens/authentication/app_logo.dart
@@ -7,17 +7,63 @@ class AppLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      key: Key('$key'),
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          vertical: 8.0,
-          horizontal: 94.0,
+    return Card(
+      color: Theme.of(context).colorScheme.onSurface,
+      elevation: 10,
+      margin: EdgeInsets.zero,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
         ),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(logo),
-          ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Icon(
+          Icons.face,
+          size: 60,
+          color: Theme.of(context).colorScheme.surface,
+        ),
+      ),
+    );
+  }
+}
+
+class AppBarCard extends StatelessWidget implements PreferredSizeWidget {
+  const AppBarCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size(double.infinity, 250);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.primary,
+      elevation: 10,
+      margin: EdgeInsets.zero,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(40),
+          bottomRight: Radius.circular(40),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 30, bottom: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const AppLogo(),
+            const SizedBox(height: 10,),
+            Text(
+              'Affirtude',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          ],
         ),
       ),
     );
